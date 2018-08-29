9bb89a7b5fb5ff16e9d6e3651f2c4e7a896656c7
Pantheo <pantheo.at@gmail.com>
2018-08-30 00:02:38 +0530

    Added the initial code Also added Ui for signin

---
+++ app/src/main/java/com/people_patterns/livingheritage/base/BaseActivity.java
@@ -0,0 +1,28 @@
+package com.people_patterns.livingheritage.base;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+public class BaseActivity extends AppCompatActivity {
+
+    public void showNothing() {
+
+    }
+
+
+    public BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+}
---
+++ app/src/main/java/com/people_patterns/livingheritage/base/BaseApplication.java
@@ -0,0 +1,7 @@
+package com.people_patterns.livingheritage.base;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+}
