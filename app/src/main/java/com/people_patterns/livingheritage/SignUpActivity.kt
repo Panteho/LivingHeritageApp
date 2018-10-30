64b8b9bb4b01897bd11a3b3eac437e7d945cb3a6
rahulverlekar <rahulv>
2018-10-30 23:52:58 +0530

    Bugs and Ui issues

--- app/src/main/java/com/people_patterns/livingheritage/SignUpActivity.kt
+++ app/src/main/java/com/people_patterns/livingheritage/SignUpActivity.kt
@@ -23,10 +23,10 @@ class SignUpActivity : BaseActivity() {
             var phone = findViewById<EditText>(R.id.edt_phone).text.toString()
             var password = findViewById<EditText>(R.id.edt_password).text.toString()
             var repassword = findViewById<EditText>(R.id.edt_repassword).text.toString()
-//            if (!password.equals(repassword)) {
-//                findViewById<EditText>(R.id.edt_repassword).setError("Enter matching passwords")
-//                return@setOnClickListener
-//            }
+            if (!password.equals(repassword)) {
+                findViewById<EditText>(R.id.edt_repassword).setError("Enter matching passwords")
+                return@setOnClickListener
+            }
 
             //When its valid
             createUser(name, email, phone, password);
@@ -36,15 +36,22 @@ class SignUpActivity : BaseActivity() {
     private fun createUser(name: String, email: String, phone: String, password: String) {
         showProgress()
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{
-            Toast.makeText(this, it.result.user.email, Toast.LENGTH_SHORT)
-            val newUser = User(name, email, phone, it.result.user.uid)
-            saveUserToDb(newUser)
+            hideProgress()
+            if(it.isSuccessful) {
+                val newUser = User(name, email, phone, it.result.user.uid)
+                saveUserToDb(newUser)
+            }
+            else {
+                if(it.exception != null) {
+                    Toast.makeText(this, it.exception!!.localizedMessage, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
     private fun saveUserToDb(newUser: User) {
         createUser(newUser, {
-            Toast.makeText(this, "User created with id " + newUser.email, Toast.LENGTH_SHORT)
+            Toast.makeText(this, "User created with id " + newUser.email, Toast.LENGTH_SHORT).show()
             hideProgress()
             finish()
         })
